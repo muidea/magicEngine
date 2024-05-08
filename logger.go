7ceab68f024d3045a9e7438897e62a16e8276181
muidea <rangh@foxmail.com>
2024-05-08 18:45:49 +0800

    feat(): 日志输出时增加请求序号

--- logger.go
+++ logger.go
@@ -30,7 +30,7 @@ func (s *logger) Handle(ctx RequestContext, res http.ResponseWriter, req *http.R
 	s.serialNo++
 
 	if EnableTrace() {
-		logPtr.Printf("Started-%d %s %s for %s", s.serialNo, req.Method, req.URL.Path, addr)
+		logPtr.Printf("Started-%05d %s %s for %s", s.serialNo, req.Method, req.URL.Path, addr)
 	}
 
 	rw := res.(ResponseWriter)
@@ -38,8 +38,8 @@ func (s *logger) Handle(ctx RequestContext, res http.ResponseWriter, req *http.R
 
 	elapseVal := time.Since(start)
 	if EnableTrace() {
-		logPtr.Printf("Completed-%d %v %s in %v", s.serialNo, rw.Status(), http.StatusText(rw.Status()), elapseVal)
+		logPtr.Printf("Completed-%05d %v %s in %v", s.serialNo, rw.Status(), http.StatusText(rw.Status()), elapseVal)
 	} else if elapseVal >= GetElapseThreshold() {
-		logPtr.Printf("Handle-%d %s %s for %s %v in %v", s.serialNo, req.Method, req.URL.Path, addr, rw.Status(), elapseVal)
+		logPtr.Printf("Handle-%05d %s %s for %s %v in %v", s.serialNo, req.Method, req.URL.Path, addr, rw.Status(), elapseVal)
 	}
 }
