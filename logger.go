99abb3502f9b53af2c8745a34a403d410c8d1e94
muidea <rangh@foxmail.com>
2024-05-08 18:45:04 +0800

    feat(): 日志输出时增加请求序号

--- logger.go
+++ logger.go
@@ -7,6 +7,7 @@ import (
 )
 
 type logger struct {
+	serialNo int64
 }
 
 func (s *logger) Handle(ctx RequestContext, res http.ResponseWriter, req *http.Request) {
@@ -26,8 +27,10 @@ func (s *logger) Handle(ctx RequestContext, res http.ResponseWriter, req *http.R
 		}
 	}
 
+	s.serialNo++
+
 	if EnableTrace() {
-		logPtr.Printf("Started %s %s for %s", req.Method, req.URL.Path, addr)
+		logPtr.Printf("Started-%d %s %s for %s", s.serialNo, req.Method, req.URL.Path, addr)
 	}
 
 	rw := res.(ResponseWriter)
@@ -35,8 +38,8 @@ func (s *logger) Handle(ctx RequestContext, res http.ResponseWriter, req *http.R
 
 	elapseVal := time.Since(start)
 	if EnableTrace() {
-		logPtr.Printf("Completed %v %s in %v", rw.Status(), http.StatusText(rw.Status()), elapseVal)
+		logPtr.Printf("Completed-%d %v %s in %v", s.serialNo, rw.Status(), http.StatusText(rw.Status()), elapseVal)
 	} else if elapseVal >= GetElapseThreshold() {
-		logPtr.Printf("Handle %s %s for %s %v in %v", req.Method, req.URL.Path, addr, rw.Status(), elapseVal)
+		logPtr.Printf("Handle-%d %s %s for %s %v in %v", s.serialNo, req.Method, req.URL.Path, addr, rw.Status(), elapseVal)
 	}
 }
